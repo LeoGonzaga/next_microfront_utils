109d8cadc648833d97afd311bf714d79a5ea1084
Leo Gonzaga <sacredlotus05@gmail.com>
2022-08-19 14:16:25 -0300

    feat: creating initial function to expose

--- next.config.js
+++ next.config.js
@@ -1,7 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  distDir: 'build',
+  webpack: (config, options) => {
+    config.plugins.push(
+      new options.webpack.container.ModuleFederationPlugin({
+        name:"mf2",
+        filename: "static/chunks/remoteEntry_2.js",
+        exposes: {
+          "./squareRoot": "./src/utils/getSquareRoot"
+        },
+        shared: [
+          {
+            react: {
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            }
+          },
+          {
+            "react-dom": {
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            }
+          },
+        ]
+      })
+    )
+    return config
+  }
 }
 
 module.exports = nextConfig
---
+++ src/utils/getSquareRoot.ts
@@ -0,0 +1,5 @@
+const squareRoot = (number: number) => {
+  return Math.sqrt(number);
+};
+
+export default squareRoot;
